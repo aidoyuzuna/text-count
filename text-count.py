46b1cf71e5473a699f3a121293644a1f32e6e8bd
saro <netsv1643+gi@gmail.com>
2023-09-17 13:04:35 +0900

    昔作ったプログラムをサルベージ

---
+++ text-count.py
@@ -0,0 +1,31 @@
+import pyperclip
+import re
+
+paste_text = pyperclip.paste()
+result_text = paste_text.replace('-','').replace('#','').replace(' ','').replace('\n','').replace('\r','').replace('\t','').replace('>','').replace('','')
+print('\n合計文字数　　　　　：'+ str(len(result_text)) +'\n')
+
+hirakana = re.sub('[^\u3041-\u309F\u30A1-\u30FF]','',result_text)
+hirakana_percent = len(hirakana) / len(result_text)
+print('ひらカナ使用率　　　：' + str('{:.0%}'.format(hirakana_percent)))
+
+kanji = re.sub('[^\u4E00-\u9FFF]','',result_text)
+kanji_percent = len(kanji) / len(result_text)
+print('漢字使用率　　　　　：' + str('{:.0%}'.format(kanji_percent)))
+
+alphabet = re.sub('[^\u0041-\u007A]','',result_text)
+alphabet_percent = len(alphabet) / len(result_text)
+print('アルファベット使用率：' + str('{:.0%}'.format(alphabet_percent)))
+
+kigou = re.sub('[\u3041-\u309F\u30A1-\u30FF\u4E00-\u9FFF\u0041-\u007A]','',result_text)
+kigou_percent = len(kigou) / len(result_text)
+print('記号使用率　　　　　：' + str('{:.0%}'.format(kigou_percent)) +'\n')
+
+if kanji_percent <= 0.19 :
+    print('漢字が少ないよ\n')
+
+elif kanji_percent >= 0.20 and kanji_percent <= 0.30 :
+    print('漢字率がちょうどいいよ\n')
+
+else:
+    print('漢字が多いよ\n')
